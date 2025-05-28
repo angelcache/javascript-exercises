8af60392235f567e55eca88f845e97914b4b6fb1
Angela <angdkay@gmail.com>
2025-05-28 15:19:23 -0600

    finish 06 - repeat string

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  newString = "";
+  for (i = 0; i < num; i++) {
+    newString += string;
+  }
+  return newString;
 };
 
 // Do not edit below this line
