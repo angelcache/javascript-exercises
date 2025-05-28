8430ab0212ba19b1b678838c0bb8e1ff71d9fff6
Angela <angdkay@gmail.com>
2025-05-28 15:42:42 -0600

    Finish 09-sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  sum = 0;
+  console.log(sum);
+  for (i = num1; i <= num2; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
