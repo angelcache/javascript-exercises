64b898de525a46b808e7ed440161a9a53ef56fa7
Angela <angdkay@gmail.com>
2025-06-07 16:07:30 -0600

    Add palindrome solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let right = 0
+  let left = str.length - 1
+
+  while (right < left) {
+    // Check if it's letter or a digit
+    if (!isAlphaorDigit(str[right])) {
+      right++;
+    } else if (!isAlphaorDigit(str[left])) {
+      left--;
+    } else {
+      if (str[right].toLowerCase() != str[left].toLowerCase()) {
+        return false;
+      }
+      right++;
+      left--;
+    }
+  }
+  return true;
+
+  function isAlphaorDigit(str) {
+    return /^[a-zA-Z0-9]$/.test(str);
+  }
 
 };
 
