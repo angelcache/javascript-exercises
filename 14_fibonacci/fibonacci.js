e931a831996c113547943240502605944ee546f4
Angela <angdkay@gmail.com>
2025-06-07 16:22:23 -0600

    Add Fibonacci solution

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+  // Check if its a string
+  if (typeof num === "string") {
+    num = parseInt(num);
+  }
+
+  // Check if its a negative value
+  if (num < 0) {
+    return "OOPS";
+  }
+
+  // Base cases when fibonacci(0) = 0 and fibonacci(1) = 1
+  if (num === 0) {
+    return 0;
+  }
+  if (num === 1) {
+    return 1;
+  }
+
+  // recursive step
+  return fibonacci(num-1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
