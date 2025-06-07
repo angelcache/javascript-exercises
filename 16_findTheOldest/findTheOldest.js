c6e8681cf3cffe446ee3f5e69362e5a7b2bc4ce7
Angela <angdkay@gmail.com>
2025-06-07 16:46:02 -0600

    Add Find the Oldest Age solution

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  let oldest = arr.reduce(([oldestPerson, oldestAge], person) => {
 
+    if (person.yearOfDeath == undefined) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+
+    const personAge = person.yearOfDeath - person.yearOfBirth;
+    if (personAge > oldestAge) {
+      return [person, personAge]; // returns this person as the new oldest
+    } else {
+      return [oldestPerson, oldestAge]; // keep old accumulater
+    }
+  }
+  , ['', 0])
+  return oldest[0]; // Returns oldest Persons name
 };
 
 // Do not edit below this line
