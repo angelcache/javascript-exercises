79a370fc7781114ed0d97a6e0457fa7bbe2d5973
Angela <angdkay@gmail.com>
2025-06-07 15:48:20 -0600

    Add calculator.js solution

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celcius = ((fahrenheit - 32) * (5/9))
+  return celcius === 0? 0: celcius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  fahrenheit = (celcius * (9/5) + 32).toFixed(1);
+  return fahrenheit === 0? 0: fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(first, second) {
+	return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, currVal) => sum += currVal, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((multiply, val) => multiply *= val, 1);
 };
 
-const power = function() {
-	
+const power = function(first, second) {
+  return first ** second;
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+  let fact = 1;
+  for (i = val; i > 0; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
